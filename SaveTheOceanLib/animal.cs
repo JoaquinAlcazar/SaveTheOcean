183d27b02e7cd61d75d23fe3245198263f77047e
JoaquinAlcazar <104784400+JoaquinAlcazar@users.noreply.github.com>
2024-03-03 21:46:46 +0100

    Las librerías de SaveTheOcean

---
+++ SaveTheOceanLib/animal.cs
@@ -0,0 +1,97 @@
+﻿namespace SaveTheOceanLib
+{
+    public class animal
+    {
+        public string Sfamily { get; set; } 
+        public string Family { get; set; }
+        public string Name { get; set; }
+        public int AffectDegree { get; set; }
+
+        public animal() { 
+            this.Sfamily = "Tortuga Marina";
+            this.Name = "tortuga";aa
+            this.Family = "tortuga";
+            this.AffectDegree = 0;
+        }
+
+        public void generateSfamily()
+        {
+            Random RNG = new Random();
+            int select = RNG.Next(0, 3);
+
+            switch (select)
+            {
+                case 0: this.Sfamily = "Tortuga Marina";
+                    break;
+                case 1:
+                    this.Sfamily = "Au Marina";
+                    break;
+                case 2:
+                    this.Sfamily = "Cetaci";
+                    break;
+            }
+            
+        }
+
+        public void generateName()
+        {
+            Random RNG = new Random();
+            string[] names = { "Falafel", "Flaffy", "Scrumba", "Brimblo", "Scrimblo", 
+            "Scrimblo", "Theresa", "Baldurs gate 3", "Nuclear Reactor", "Thing", "Holy bingle", "Baron fofo",
+            "Crispy", "Izutsumi"};
+            this.Name = names[RNG.Next(0, names.Length)];
+        }
+
+        public void generateFamily()
+        {
+            Random RNG = new Random();
+            string[] familiesTM = { "Tortuga llaüt", "Tortuga lora", "Tortuga golfina", "Tortuga caguama",
+            "Tortuga carey", "Tortuga del pacífic"};
+            string[] familiesAM = { "Corb mari", "Corb monyut", "Alcatraz comu", "Pardela", 
+            "Gavina reidora", "Alca comú", "Fraret atlantic"};
+            string[] familiesC = { "Balena", "Dofi", "Peix", "Cefalopod" };
+
+            if (this.Sfamily == "Tortuga Marina")
+            {
+                this.Family = familiesTM[RNG.Next(0,familiesTM.Length)];
+            } else if(this.Sfamily == "Au Marina")
+            {
+                this.Family = familiesAM[RNG.Next(0, familiesAM.Length)];
+            }
+            else
+            {
+                this.Family = familiesC[RNG.Next(0, familiesC.Length)];
+            }
+                       
+        }
+
+        public void setAD()
+        {
+            Random rnd = new Random();
+            this.AffectDegree = rnd.Next(1, 100);
+        }
+
+        public int treat(string loc)
+        {
+            int x = 5;
+
+            if (this.Sfamily == "Tortuga Marina")
+            {
+                this.AffectDegree = this.AffectDegree - ((this.AffectDegree - 2) * (2 * this.AffectDegree + 3)) - x;
+            } else if (this.Sfamily == "Au Marina")
+            {
+                if (loc == "CRAM") x = 0;
+                this.AffectDegree = this.AffectDegree - ((this.AffectDegree * this.AffectDegree) + x);
+            }
+            else
+            {               
+                x = 25;
+                if (loc == "CRAM") x = 0;
+                double affectedDlog = Math.Log10(this.AffectDegree);
+                this.AffectDegree = this.AffectDegree - Convert.ToInt32(affectedDlog) - x;
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ SaveTheOceanLib/player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveTheOceanLib
+{
+    public class player
+    {
+        protected type playerType { get; set; }
+        public int experience { get; set; } = 0;
+        public string name { get; set; }
+
+        public player()
+        {
+            this.playerType = type.tecnic;
+            this.experience = 0;
+            this.name = "Placeholder";
+        }
+        public void selectType()
+        {
+            int selection = 0;
+            Console.WriteLine("Selecciona el teu personatje \n 1. Veterinari\n2. Tecnic");
+            while (selection < 1 || selection > 2) {
+                selection = Convert.ToInt32(Console.ReadLine());
+                switch (selection)
+                {
+                    case 1: this.playerType = type.veterinari; break;
+                    case 2: this.playerType = type.tecnic; break;
+                    default: Console.WriteLine("Error, selecciona una altra vegada"); break;
+                }
+            }
+        }
+
+        public void selectName()
+        {
+            Console.WriteLine("Selecciona el teu nom");
+            this.name = Console.ReadLine();
+        }
+    }
+}
